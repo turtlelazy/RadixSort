0e11dfa8f90f97870497420bc2df4b753f12d7b7
turtlelazy <ishraqmahid@gmail.com>
2021-01-12 12:07:10 -0500

    nth method created

--- Radix.java
+++ Radix.java
@@ -1,3 +1,14 @@
 public class Radix{
-    
+    public static int nth(int n, int col){
+        int modulo = n + 0; //copying without reference
+        int power = 1;
+
+        for(int i = 0; i < col-1; i++){
+            power *= 10;
+        }
+        modulo /= power;
+
+        return modulo % 10;
+    }
+
 }
