497d008e81b14ecbb164805b4d4d577531b14772
turtlelazy <ishraqmahid@gmail.com>
2021-01-12 12:10:53 -0500

    index definition adjusted for nth method

--- Radix.java
+++ Radix.java
@@ -3,7 +3,7 @@ public class Radix{
         int modulo = n + 0; //copying without reference
         int power = 1;
 
-        for(int i = 0; i < col-1; i++){
+        for(int i = 0; i < col; i++){
             power *= 10;
         }
         modulo /= power;
