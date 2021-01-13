93a9fc6750634d31bc8030eaee017c23b8750bff
turtlelazy <ishraqmahid@gmail.com>
2021-01-13 10:27:31 -0500

    created length function

--- Radix.java
+++ Radix.java
@@ -11,4 +11,18 @@ public class Radix{
         return modulo % 10;
     }
 
+    public static int length(int n){
+        int a = n + 0;
+        if(n<0){
+            n *= -1;
+        }
+        int length = 1;
+        while(n>9){
+            length ++;
+            a/= 10;
+        }
+
+        return length;
+    }
+
 }
