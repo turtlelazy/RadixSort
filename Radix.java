f76a11a001142002edb0de6ade663e7916d2ea43
turtlelazy <ishraqmahid@gmail.com>
2021-01-13 17:52:46 -0500

    created constant time length and nth methods

--- Radix.java
+++ Radix.java
@@ -1,41 +1,15 @@
 public class Radix{
     public static int nth(int n, int col){
-        /*
-        //Original nth method
-        int modulo = n + 0; //copying without reference
-        int power = 1;
+        if (n < 0) {n *= -1;}
+        int place = (int) Math.pow(10, col);
+        return (n / place)%10; 
 
-        for(int i = 0; i < col; i++){
-            power *= 10;
-        }
-        modulo /= power;
-
-        return modulo % 10;
-        */
-
-        //new nth method although unsure if it works that well
-        int input = n + 0;
-        if(input < 0) {input *= -1;}
-        String numberAsString = input + "";
-        char digit = numberAsString.charAt(numberAsString.length()-(col+1));
-        int returnDigit = digit - 48;
-
-        return returnDigit;
     }
 
     public static int length(int n){
-        int a = n + 0;
-        
-        if(a<0){
-            a *= -1;
-        }
-        int length = 1;
-        while(a>9){
-            length ++;
-            a/= 10;
-        }
-
-        return length;
+        if(n == 0) {return 1;}
+        if(n<0) {n *= -1;}
+        return (int) Math.log10(n)+1;
     }
 
     public static void merge(MyLinkedList original, MyLinkedList[] buckets){
